669b2c2bb104cf64f970ea8718f82578725cfd66
Mahesh V <123245967+Mahesh-Vijaykumar@users.noreply.github.com>
2024-10-26 09:33:11 +0530

    swap_string.c

---
+++ My-initial_pull/swap_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+str a,b,temp;  
+printf("Enter string 1:\n");
+scanf("%s" &a);
+printf("Enter string 2:\n");
+scanf("%s" &b);
+printf("strings before swapping : a = %s b = %s "),a,b;
+a = temp ;
+b = a ;
+temp = b ;
+printf("strings after swapping : a = %s b = %s " , a , b);
+}
